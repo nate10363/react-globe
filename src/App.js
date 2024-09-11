700741502f3ad663b09325cef6929f186f140d40
nate <natedavenport103@gmail.com>
2024-09-10 22:07:27 -0400

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from "react";
+import World from "./World";
+
+function App() {
+  return (
+    <div className="App">
+      <World />
+    </div>
+  );
+}
+
+export default App;
